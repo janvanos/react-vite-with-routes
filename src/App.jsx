a3e0dc020370ad8a7d526333e7adb371d048ea2f
Jan van Os <jos@glu.nl>
2024-01-17 10:14:26 +0100

    vite React with routes

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import Login from './pages/Login';
+import './App.css';
+
+function App() {
+
+  return (
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </Router>
+  );
+}
+
+export default App;
---
+++ src/Home.jsx
@@ -0,0 +1,10 @@
+
+function Home() {
+  return (
+    <>
+     <div>test</div>
+    </>
+  )
+}
+
+export default Home
